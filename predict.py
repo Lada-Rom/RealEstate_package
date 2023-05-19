8a83fa439cc0dbb7b4623b4d99e695d91686a6b8
Lada-Rom <doodleztoon@gmail.com>
2023-05-19 21:43:27 +0300

    NEW: model and code example how to use it

---
+++ predict.py
@@ -0,0 +1,21 @@
+import lightgbm as lgb
+import pandas as pd
+
+# geo_lat float
+# geo_lon float
+# region uint categoric
+# level uint
+# levels uint
+# rooms uint
+# area float
+# object_type uint categoric
+def predict(geo_lat, geo_lon, region, level, levels, rooms, area, object_type):
+    model = lgb.Booster(model_file = "/kaggle/input/real-estate-weights/lgb_model.txt")
+    
+    data = {'geo_lat': geo_lat, 'geo_lon': geo_lon, 'region': region, 'level': level,
+            'levels': levels, 'rooms': rooms, 'area': area, 'object_type': object_type}
+    case = pd.DataFrame(data=data, index=[0])
+    prediction = model.predict(case)
+    return prediction[0]
+    
+ predict(59.805808, 30.376141, 2661, 8, 10, 3, 82.6, 1)
